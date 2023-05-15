5bb8ec6156e9070b4b64dc6dcf8a172f8b32bb2c
Dylan <drnoble888@gmail.com>
2023-05-15 16:35:55 -0600

    Working yml

--- parkpal_service/migrations/001_sample_migration.py
+++ parkpal_service/migrations/001_sample_migration.py
@@ -1,5 +1,20 @@
 steps = [
     [
+        """
+        CREATE TABLE appointments (
+            id SERIAL PRIMARY KEY NOT NULL,
+            national_park_name VARCHAR(50) NOT NULL,
+            start_date: TIMESTAMP NOT NULL,
+            end_date: TIMESTAMP NOT NULL,
+            activites: TEXT NOT NULL,
+        );
+        """,
+        """
+        DROP TABLE appointments;
+        """
+    ],
+        
+    [
         # "Up" SQL statement
         """
         CREATE TABLE dummy (
