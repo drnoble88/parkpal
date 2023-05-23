d19d76b145e172e4fd17b5522ca7db5aa531649d
Dylan <drnoble888@gmail.com>
2023-05-23 15:37:01 -0600

    Get one and all parks

--- parkpal_service/queries/national_parks.py
+++ parkpal_service/queries/national_parks.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from typing import List
 import requests
+from queries.pool import pool
+import ast
 # from env.sample import API_KEY
 
 
@@ -14,31 +16,119 @@ class NationalPark(BaseModel):
     images: list
     parkCode: str
 
+class NationalParkOut(NationalPark):
+    id: str
 
+
+    
 class NationalParkAPIQueries:
-    def get_all_national_parks(self) -> List[NationalPark]:
-        res = requests.get('https://developer.nps.gov/api/v1/parks?limit=40&api_key=sddc5G1qLchZwcSUwLr1dA2NTiaXvMs5yrOS41jE')
+    def seeding_all_national_parks(self) -> List[NationalPark]:
+        res = requests.get('https://developer.nps.gov/api/v1/parks?limit=469&api_key=sddc5G1qLchZwcSUwLr1dA2NTiaXvMs5yrOS41jE')
         data = res.json()
         real_data = data['data']
         new_data = []
         all_images = []
-        phone_number = 'No phone number found'
+        list_activities = []
+        phone_number = "No phone number found"
+        address = {}
         for entry in real_data:
             for index in range(len(entry["images"])):
                 all_images.append(entry["images"][index]["url"])
+            for index in range(len(entry["activities"])):
+                list_activities.append(entry["activities"][index]["name"])
             if len(entry["contacts"]["phoneNumbers"]) != 0:
                 phone_number = entry["contacts"]["phoneNumbers"][0]["phoneNumber"]
+            if len(entry["addresses"]) != 0:
+                address = entry["addresses"][0]
             national_park = NationalPark(
                 fullName=entry["fullName"],
-                activities=entry["activities"],
+                activities= list_activities,
                 description=entry["description"],
                 phoneNumber=phone_number,
                 emailAddresses=entry["contacts"]["emailAddresses"][0]["emailAddress"],
-                addresses=entry["addresses"][0],
+                addresses= address,
                 images=all_images,
                 parkCode=entry["parkCode"]
             )
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute (
+                        """
+                        INSERT INTO nationalparks
+                        (fullName, activities, description, phoneNumber, emailAddresses, addresses, images, parkCode)
+                        VALUES
+                        (%s, %s, %s, %s, %s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            str(national_park.fullName),
+                            str(national_park.activities),
+                            str(national_park.description),
+                            str(national_park.phoneNumber),
+                            str(national_park.emailAddresses),
+                            str(national_park.addresses),
+                            str(national_park.images),
+                            str(national_park.parkCode)
+                        ]
+                    )
+
             new_data.append(national_park)
             all_images = []
-            phone_number = 'No phone number found'
+            list_activities = []
+            phone_number = "No phone number found"
+            address = {}
+
         return new_data
+
+
+    def get_one_national_park(self, park_code: str) -> NationalParkOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    SELECT id, fullName, activities, description, phoneNumber, emailAddresses, addresses, images, parkCode
+                    FROM nationalparks
+                    WHERE parkCode = %s;
+                    """,
+                    [park_code]
+                )
+                record = result.fetchone()
+                if record:
+                    return NationalParkOut(
+                        id= record[0],
+                        fullName= record[1],
+                        activities= ast.literal_eval(record[2]),
+                        description= record[3],
+                        phoneNumber= record[4],
+                        emailAddresses= record[5],
+                        addresses= ast.literal_eval(record[6]),
+                        images= ast.literal_eval(record[7]),
+                        parkCode= record[8]
+                    )
+
+
+    def list_all_national_parks(self) -> List[NationalParkOut]:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id, fullName, activities, description, phoneNumber, emailAddresses, addresses, images, parkCode
+                        FROM nationalparks;
+                        """
+
+                    )
+                    result = db.fetchall()
+                    return [
+                        NationalParkOut(
+                            id= record[0],
+                            fullName= record[1],
+                            activities= ast.literal_eval(record[2]),
+                            description= record[3],
+                            phoneNumber= record[4],
+                            emailAddresses= record[5],
+                            addresses= ast.literal_eval(record[6]),
+                            images= ast.literal_eval(record[7]),
+                            parkCode= record[8]
+                        )
+                        for record in result
+                    ]
--- parkpal_service/routers/national_parks.py
+++ parkpal_service/routers/national_parks.py
@@ -1,12 +1,25 @@
 from fastapi import APIRouter, Depends
-from queries.national_parks import NationalPark, NationalParkAPIQueries
+from queries.national_parks import NationalPark, NationalParkAPIQueries, NationalParkOut
 from typing import List
 
 router = APIRouter()
 
 
-@router.get("/api/nationalparks", response_model=List[NationalPark])
-def get_all_national_parks(
+@router.get("/api/seednationalparks", response_model=List[NationalPark])
+def seeding_all_national_parks(
     repo: NationalParkAPIQueries = Depends(),
 ): 
-    return repo.get_all_national_parks()    
+    return repo.seeding_all_national_parks()    
+
+@router.get("/api/nationalparks/{park_code}", response_model=NationalParkOut)
+def get_one_national_park(
+    park_code,
+    repo: NationalParkAPIQueries = Depends(),
+): 
+    return repo.get_one_national_park(park_code)    
+
+@router.get("/api/nationalparks", response_model=List[NationalParkOut])
+def list_all_national_parks(
+    repo: NationalParkAPIQueries = Depends(),
+): 
+    return repo.list_all_national_parks()    
