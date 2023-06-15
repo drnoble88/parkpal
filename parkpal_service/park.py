c9ac8f1e14e09855fb458fd74acb70cba4dbd832
Dylan <drnoble888@gmail.com>
2023-06-15 15:30:54 -0600

    ci/cd

--- parkpal_service/park.py
+++ parkpal_service/park.py
@@ -7,7 +7,7 @@ def seeding_all_national_parks():
     api_key = 'sddc5G1qLchZwcSUwLr1dA2NTiaXvMs5yrOS41jE'
     res = requests.get(
         f'https://developer.nps.gov/api/v1/parks?limit=469&api_key={api_key}'
-        )
+    )
     data = res.json()
     print(data['data'][0]['contacts']['phoneNumbers'])
     real_data = data['data']
@@ -16,28 +16,39 @@ def seeding_all_national_parks():
     list_activities = []
     phone_number = "No phone number found"
     address = {}
-    for entry in real_data:
-        for index in range(len(entry["images"])):
-            all_images.append(entry["images"][index]["url"])
-        for index in range(len(entry["activities"])):
-            list_activities.append(entry["activities"][index]["name"])
-        if len(entry["contacts"]["phoneNumbers"]) != 0:
-            phone_number = entry["contacts"]["phoneNumbers"][0]["phoneNumber"]
-        if len(entry["addresses"]) != 0:
-            address = entry["addresses"][0]
-        national_park = NationalPark(
-            fullName=entry["fullName"],
-            activities=list_activities,
-            description=entry["description"],
-            phoneNumber=phone_number,
-            emailAddresses=entry["contacts"]["emailAddresses"]
-            [0]["emailAddress"],
-            addresses=address,
-            images=all_images,
-            parkCode=entry["parkCode"]
-        )
-        with pool.connection() as conn:
-            with conn.cursor() as db:
+
+    with pool.connection() as conn:
+        with conn.cursor() as db:
+            db.execute("SELECT parkCode FROM nationalparks;")
+            existing_park_codes = {row[0] for row in db.fetchall()}
+
+            for entry in real_data:
+                park_code = entry["parkCode"]
+                if park_code in existing_park_codes:
+                    print(f"Skipping seeding for park code: {park_code}")
+                    continue
+
+                for index in range(len(entry["images"])):
+                    all_images.append(entry["images"][index]["url"])
+                for index in range(len(entry["activities"])):
+                    list_activities.append(entry["activities"][index]["name"])
+                if len(entry["contacts"]["phoneNumbers"]) != 0:
+                    phone_number = (entry["contacts"]["phoneNumbers"]
+                                    [0]["phoneNumber"])
+                if len(entry["addresses"]) != 0:
+                    address = entry["addresses"][0]
+                national_park = NationalPark(
+                    fullName=entry["fullName"],
+                    activities=list_activities,
+                    description=entry["description"],
+                    phoneNumber=phone_number,
+                    emailAddresses=(entry["contacts"]["emailAddresses"]
+                                    [0]["emailAddress"]),
+                    addresses=address,
+                    images=all_images,
+                    parkCode=park_code
+                )
+
                 db.execute(
                     """
                     INSERT INTO nationalparks
@@ -59,12 +70,12 @@ def seeding_all_national_parks():
                     ]
                 )
 
-        new_data.append(national_park)
-        all_images = []
-        list_activities = []
-        phone_number = "No phone number found"
-        address = {}
-        print("seed successful")
+                new_data.append(national_park)
+                all_images = []
+                list_activities = []
+                phone_number = "No phone number found"
+                address = {}
+                print("Seed successful")
 
 
 seeding_all_national_parks()
